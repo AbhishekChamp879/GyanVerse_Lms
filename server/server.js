e16ac003ba57d4494244e937b8946083cf1536c4
AbhishekChamp879 <abhishekkumargiri770@gmail.com>
2026-05-08 13:15:41 +0530

    feat: add CourseProgress model, update package dependencies, and implement course routes; configure server for Vercel deployment

--- server/server.js
+++ server/server.js
@@ -6,11 +6,11 @@ import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js'
 import { clerkMiddleware } from '@clerk/express'
 import educatorRouter from './routes/educatorRoutes.js'
 import connectCloudinary from './configs/cloudinary.js'
-import courseRouter from './routes/courseRoute.js'
+import courseRouter from './routes/courseRoutes.js'
 import userRouter from './routes/userRoutes.js'
 
 //initialize express
-const app= express()
+const app = express()
 //connect to database
 await connectDB()
 await connectCloudinary()
@@ -20,15 +20,15 @@ app.use(cors())
 app.use(clerkMiddleware())
 
 //Routes
-app.get('/', (req,res)=>res.send("API Working"))
+app.get('/', (req, res) => res.send("API Working"))
 app.post('/clerk', express.json(), clerkWebhooks)
 
-app.use('/api/educator',express.json(),educatorRouter)
-app.use('/api/course',express.json(),courseRouter)
-app.use('/api/user',express.json(), userRouter)
-app.post('/stripe', express.raw({type:'application/json'}), stripeWebhooks)
+app.use('/api/educator', express.json(), educatorRouter)
+app.use('/api/course', express.json(), courseRouter)
+app.use('/api/user', express.json(), userRouter)
+app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks)
 //port
 const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
