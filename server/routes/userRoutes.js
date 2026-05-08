e84d6284ad31010c8441cb2e2927cbf6cb95e5fd
AbhishekChamp879 <abhishekkumargiri770@gmail.com>
2026-05-08 22:13:35 +0530

    feat: initialize backend server architecture including database, cloud storage, course models, and educator/user management routes.

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,12 +4,12 @@ import { addUserRating, getUserCourseProgress, getUserData, purchaseCourse, upda
 const userRouter = express.Router()
 
 userRouter.get('/data', getUserData)
-userRouter.get('/enrolled-courses',userEnrolledCourses)
-userRouter.post('/purchase',purchaseCourse)
+userRouter.get('/enrolled-courses', userEnrolledCourses)
+userRouter.post('/purchase', purchaseCourse)
 
 userRouter.post('/update-course-progress', updateUserCourseProgress)
-userRouter.post('/get-course-progress',getUserCourseProgress)
-userRouter.post('/add-rating',addUserRating)
+userRouter.post('/get-course-progress', getUserCourseProgress)
+userRouter.post('/add-rating', addUserRating)
 
 
 export default userRouter;
